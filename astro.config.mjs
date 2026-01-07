4960b9f3a382dc9fa8ae4b8d4f1d31719f046044
Nicole Tin <nicolelrtin@gmail.com>
2026-01-06 20:57:24 -0800

    Configure deployment settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
+```javascript
 // @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://nicolelrtin.github.io',
+  // base is not needed for user.github.io repositories as they are served from root
+});
+```
